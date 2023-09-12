5d8961353869eebdf7e3a7e95dadd50f0d6f6179
Michael Dratch <michael.s.dratch@gmail.com>
2023-09-12 12:56:42 -0400

    port table transformation

--- pipe-line/transform/transform.py
+++ pipe-line/transform/transform.py
@@ -1,7 +1,8 @@
 class Transformer:
 
     def make_tables(self, headers_df, cargo_descriptions_df, tariffs_df):
-        self.make_ports_table(headers_df)
+        self.make_domestic_ports_table(headers_df)
+        self.make_foreign_ports_table(headers_df)
         self.make_vessels_table(headers_df)
         self.make_voyages_table(headers_df)
         self.make_shipments_table(headers_df)
@@ -9,11 +10,38 @@ class Transformer:
         self.cargo_items_table(cargo_descriptions_df)
         self.make_tariffs_table(tariffs_df)
 
-    def make_ports_table(self, headers_df):
-        pass
+    def make_domestic_ports_table(self, headers_df):
+        ports = headers_df.groupby("port_of_unlading").count()
+        ports.reset_index()
+        ports = ports[ports['port_of_unlading'].str.contains('[a-z,A-Z]')]
+        ports = self.split_port_name_into_seperate_fields(ports)
+        ports = ports[['city', 'state']]
+        return ports
+
+    def make_foreign_ports_table(self, headers_df):
+        ports = headers_df.groupby("port_of_unlading").count()
+        ports.reset_index()
+        ports = ports[ports['port_of_unlading'].str.contains('[a-z,A-Z]')]
+        ports = self.split_port_name_into_seperate_fields(
+            ports, isDomestic=False)
+        ports = ports[['city', 'country']]
+        return ports
+
+    def split_port_name_into_seperate_fields(self, ports_df, isDomestic=True):
+        label = 'country'
+        if isDomestic:
+            label = 'state'
+        ports_df[label] = ports_df['port_of_unlading'].astype(
+            'string').apply(lambda name: name.split(',')[-1])
+        ports_df['city'] = ports_df['port_of_unlading'].astype(
+            'string').apply(lambda name: name.rsplit(',', 1)[0])
+        return ports_df
 
     def make_vessels_table(self, headers_df):
-        pass
+        vessels = headers_df[['vessel_name', 'vessel_country_code']]
+        vessels = vessels.groupby(
+            ['vessel_name', 'vessel_country_code']).count().reset_index()
+        return vessels
 
     def make_voyages_table(self, headers_df):
         pass
